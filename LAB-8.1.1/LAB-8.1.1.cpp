40356b1108da6a862db63ca90e4a80c50b226682
Sushomeister <91095463+Sushomeister@users.noreply.github.com>
2022-03-30 17:22:21 +0300

    Update LAB-8.1.1.cpp

--- LAB-8.1.1/LAB-8.1.1.cpp
+++ LAB-8.1.1/LAB-8.1.1.cpp
@@ -24,57 +24,16 @@ int Count(char* str)
 	return k;
 }
 
-char* Change(char* str)
-{
-	size_t len = strlen(str);
-
-	if (len < 3)
-		return str;
-
-	char* tmp = new char[len * 4 / 3 + 1];
-	char* t = tmp;
-
-	tmp[0] = '\0';
-
-	size_t i = 0;
-
-		while (i < len && str[i + 2] != 0)
-		{
-			if (str[i] == ',' && str[i + 2] == '-')
-			{
-				strcat(t, "**");
-				t += 4;
-				i += 3;
-			}
-			else
-			{
-				*t++ = str[i++];
-				*t = '\0';
-			}
-		}
-	*t++ = str[i++];
-	*t++ = str[i++];
-	*t = '\0';
-
-	strcpy(str, tmp);
-
-	return tmp;
-}
-
 int main()
 {
+	setlocale(LC_ALL, "Ukrainian");
+
 	char str[101];
 
-	cout << "Enter string:" << endl;
+	cout << "Ввести лiтерний рядок:" << endl;
 	cin.getline(str, 100);
 
-	cout << "String contained " << Count(str) << " groups of ', -'" << endl;
-
-	char* dest = new char[151];
-	dest = Change(str);
-
-	cout << "Modified string (param) : " << str << endl;
-	cout << "Modified string (result): " << dest << endl;
+	cout << "Лiтерний рядок мiстить " << Count(str) << " групи ', -'" << endl;
 
 	return 0;
 }
